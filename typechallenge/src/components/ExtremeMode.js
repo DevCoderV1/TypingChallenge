0d20d7dcf0955050d7f2d94aafff6dece2eda5c2
M1ck3yZ <m1ck3yz@hotmail.com>
2021-05-24 19:49:41 -0400

    updating the code

--- typechallenge/src/components/ExtremeMode.js
+++ typechallenge/src/components/ExtremeMode.js
@@ -1,4 +1,107 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function ExtremeMode() {
-  return <div>Extreme Mode</div>;
+  const easyText =
+    "&R&^^T&BY(*Y(HI(&T&*DY*)Y*(@)(U)(Ujdqjweujd1j--9UJokkq-jd--j-j-jd-1j-dk1-k0dk-01jd-2j1d-1j2-dj1-m1---I_(I_)I_)Jd-qk0";
+  const [mytext, SetText] = useState("");
+  const [time, settime] = useState(0);
+  const [hide, sethide] = useState(true);
+  const NewText = easyText.split("");
+  const userText = mytext.split("");
+  const [status, setStatus] = useState(false);
+  const [message, setmessage] = useState("hello");
+
+  //runs the timer
+  useEffect(() => {
+    if (status) {
+      setInterval(() => {
+        settime((time) => time + 1);
+      }, 1000);
+    } else {
+    }
+  }, [status]);
+
+  //check if text is valid and same as displaytext
+  const checktext = () => {
+    let count = 0;
+    sethide(false);
+    if (userText.length !== NewText.length) {
+      console.log("false");
+    } else if (userText.length === NewText.length) {
+      for (let x = 0; x <= NewText.length; x++) {
+        if (userText[x] === NewText[x]) {
+          count = count + 1;
+          console.log("true");
+        } else {
+          console.log("false");
+        }
+      }
+    }
+    setmessage(`You have ${count} / ${NewText.length} text correct`);
+  };
+  const restartapp = () => {
+    window.location.reload();
+  };
+
+  const show = {
+    display: "block",
+  };
+
+  const noshow = {
+    display: "none",
+  };
+
+  return (
+    <div className="game_mode">
+      <div className="nav_bar">
+        <div className="game_title">
+          <h1>Extreme Mode</h1>
+        </div>
+        <div className="menu_button">
+          <Link to="/">
+            <button>Back to Main Menu</button>
+          </Link>
+        </div>
+      </div>
+      <div className="displaytext">
+        <p>{easyText}</p>
+      </div>
+      <div className="result">
+        <div className={hide ? "timer" : "hidetime"}>{time}</div>
+        <div className={hide ? "hideboard" : "scoreboard"}>{message}</div>
+      </div>
+      <div className="user_input">
+        <textarea
+          onChange={(e) => {
+            SetText(e.target.value);
+            console.log(mytext);
+            setStatus(true);
+          }}
+          type="text"
+          autoFocus
+          name="myinput"
+          placeholder="Type your text here..."
+          onKeyUp={(e) => {
+            let numletter = e.target.value.length;
+            let numhidden = "";
+            for (let x = 0; x < numletter; x++) {
+              numhidden += "*";
+            }
+            e.target.value = numhidden;
+          }}
+        />
+      </div>
+      <div className="submit_btn">
+        <button style={hide ? show : noshow} onClick={checktext}>
+          Submit Text
+        </button>
+        <button style={hide ? noshow : show} onClick={restartapp}>
+          Try Again
+        </button>
+      </div>
+    </div>
+  );
 }
+
 export default ExtremeMode;
