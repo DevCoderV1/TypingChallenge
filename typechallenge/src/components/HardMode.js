27e9a0c9c5f95de6bfcc3eeb6a2e11e4183d2f72
M1ck3yZ <m1ck3yz@hotmail.com>
2023-01-12 20:41:51 -0500

    Update HardMode.js

--- typechallenge/src/components/HardMode.js
+++ typechallenge/src/components/HardMode.js
@@ -1,58 +1,58 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function HardMode() {
   const Text =
-    "Duis id mi id velit vestibulum cursus. Integer tincidunt nisi felis, eu consectetur nulla dignissim id. Quisque gravida  condimentum egestas. Sed ex magna, fringilla vitae orci a, egestas elementum erat. Vestibulum @!semper imper^diet mi, vel (laoreet) libero semper eget. Mauris  sit   amet feugiat diam. Vivamus sed mattis ipsum. Aliquam venenatis efficitur? leo, et placerat ligula lacinia non!. Nam tristique tortor eu arcu  interdum  auctor.";
-  const [mytext, SetText] = useState("");
-  const [time, settime] = useState(0);
-  const [hide, sethide] = useState(true);
-  const [status, setStatus] = useState(false);
-  const [message, setmessage] = useState("");
+    'Duis id mi id velit vestibulum cursus. Integer tincidunt nisi felis, eu consectetur nulla dignissim id. Quisque gravida condimentum egestas. Sed ex magna, fringilla vitae orci a, egestas elementum erat. Vestibulum @!semper imper^diet mi, vel (laoreet) libero semper eget. Mauris  sit   amet feugiat diam. Vivamus sed mattis ipsum. Aliquam venenatis efficitur? leo, et placerat ligula lacinia non!. Nam tristique tortor eu arcu  interdum  auctor.'
+  const [mytext, SetText] = useState('')
+  const [time, settime] = useState(0)
+  const [hide, sethide] = useState(true)
+  const [status, setStatus] = useState(false)
+  const [message, setmessage] = useState('')
 
   //runs the timer
   useEffect(() => {
     if (status) {
       setInterval(() => {
-        settime((time) => time + 1);
-      }, 1000);
+        settime((time) => time + 1)
+      }, 1000)
     } else {
     }
-  }, [status]);
+  }, [status])
 
   //check if text is valid and same as displaytext
   const checktext = () => {
-    const NewText = Text.split("");
-    const userText = mytext.split("");
-    sethide(false);
-    let count = 0;
+    const NewText = Text.split('')
+    const userText = mytext.split('')
+    sethide(false)
+    let count = 0
     for (let x = 0; x < NewText.length; x++) {
       if (userText[x] === NewText[x]) {
-        count = count + 1;
+        count = count + 1
       }
     }
     setmessage(
-      `You took: ${time} seconds. You have ${count} out of ${NewText.length} character correct !`
-    );
-  };
+      `You took: ${time} seconds. You have ${count} out of ${NewText.length} character correct !`,
+    )
+  }
   //DisplayInput
 
   const DisplayInput = (e) => {
-    SetText(e.target.value);
-    setStatus(true);
-  };
+    SetText(e.target.value)
+    setStatus(true)
+  }
   //refresh the web browser
   const restartapp = () => {
-    window.location.reload();
-  };
+    window.location.reload()
+  }
 
   const show = {
-    display: "block",
-  };
+    display: 'block',
+  }
 
   const noshow = {
-    display: "none",
-  };
+    display: 'none',
+  }
 
   return (
     <div className="game_mode">
@@ -70,7 +70,7 @@ function HardMode() {
         <p>{Text}</p>
       </div>
       <div className="result">
-        <div className={hide ? "hideboard" : "scoreboard"}>{message}</div>
+        <div className={hide ? 'hideboard' : 'scoreboard'}>{message}</div>
       </div>
       <div className="user_input">
         <textarea
@@ -90,7 +90,7 @@ function HardMode() {
         </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default HardMode;
+export default HardMode
